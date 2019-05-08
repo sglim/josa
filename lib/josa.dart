e46c28d5c39f89654093dfff84383a3f4efbd76c
Seung-gi Lim <seung.gi.lim@gmail.com>
2019-05-08 14:18:48 +0900

    Implement josa library

--- lib/josa.dart
+++ lib/josa.dart
@@ -1,7 +1,104 @@
 library josa;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// JosaType
+///
+/// Type6 has a exception. If jong seong is ㄹ, result will be opposite.
+enum JosaType {
+  type1, // 을/를
+  type2, // 은/는
+  type3, // 이/가
+  type4, // 와/과
+  type5, // 이여/여
+  type6, // 으로/로
+  type7, // 이에요/예요
+  type8, // 이었/였
+  type9, // 아/야
+}
+
+class JosaGroup {
+  final JosaType type;
+  final String forClosedSyllable;
+  final String forOpenedSyllable;
+
+  JosaGroup(this.type, this.forClosedSyllable, this.forOpenedSyllable);
+
+  factory JosaGroup.of(JosaType type) {
+    switch (type) {
+      case JosaType.type1:
+        return JosaGroup(type, '을', '를');
+      case JosaType.type2:
+        return JosaGroup(type, '은', '는');
+      case JosaType.type3:
+        return JosaGroup(type, '이', '가');
+      case JosaType.type4:
+        return JosaGroup(type, '와', '과');
+      case JosaType.type5:
+        return JosaGroup(type, '이여', '여');
+      case JosaType.type6:
+        return JosaGroup(type, '으로', '로');
+      case JosaType.type7:
+        return JosaGroup(type, '이예요', '예요');
+      case JosaType.type8:
+        return JosaGroup(type, '이었', '였');
+      case JosaType.type9:
+        return JosaGroup(type, '아', '야');
+    }
+    assert(false);
+    return null;
+  }
+
+  static final List<JosaGroup> groups =
+      JosaType.values.map((type) => JosaGroup.of(type)).toList();
+
+  bool isPartOf(String value) {
+    return value.contains(forOpenedSyllable) ||
+        value.contains(forClosedSyllable);
+  }
+}
+
+class Josa {
+  final JosaGroup group;
+  final bool withValue;
+
+  Josa(this.group, this.withValue);
+
+  factory Josa.of(String josa, {bool withValue = true}) {
+    final group = JosaGroup.groups.firstWhere((group) => group.isPartOf(josa));
+    return Josa(group, withValue);
+  }
+
+  int _getLastSyllableCode(String value) {
+    int codeUnit = value.codeUnitAt(value.length - 1);
+    return (codeUnit - 0xac00) % 28;
+  }
+
+  bool _hasClosedSyllable(String value) {
+    final lastSyllable = _getLastSyllableCode(value);
+    final isClosed = lastSyllable > 0;
+    print(value);
+    print(isClosed);
+    print(group.type);
+    print(lastSyllable);
+    if (group.type == JosaType.type6 && lastSyllable == 8) {
+      // Note: 으로/로 has a exception when there is ㄹ as a last syllable
+      return !isClosed;
+    }
+    return isClosed;
+  }
+
+  String getJosaFor(String value) {
+    _getLastSyllableCode(value);
+    if (_hasClosedSyllable(value)) {
+      return group.forClosedSyllable;
+    }
+    return group.forOpenedSyllable;
+  }
+
+  String of(String value) {
+    String josa = getJosaFor(value);
+    if (withValue) {
+      return '$value$josa';
+    }
+    return josa;
+  }
 }
--- test/josa_test.dart
+++ test/josa_test.dart
@@ -3,11 +3,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:josa/josa.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('Josa type', () {
+    final josa = Josa.of('을', withValue: false);
+    expect(josa.group.forClosedSyllable, '을');
+    expect(josa.group.forOpenedSyllable, '를');
+    expect(Josa.of('을', withValue: false).group.forClosedSyllable, '을');
+    expect(Josa.of('을/를', withValue: false).group.forClosedSyllable, '을');
+    expect(Josa.of('을를 ', withValue: false).group.forClosedSyllable, '을');
+    expect(Josa.of('사람을', withValue: false).group.forClosedSyllable, '을');
+    expect(() => Josa.of('몰랑', withValue: false), throwsStateError);
+  });
+
+  test('을', () {
+    final josa = Josa.of('을', withValue: false);
+    expect(josa.of('동물원'), '을');
+    expect(josa.of('농장'), '을');
+    expect(josa.of('머리'), '를');
+  });
+
+  test('으로', () {
+    final josa = Josa.of('으로/로', withValue: false);
+    expect(josa.of('적진'), '으로');
+    expect(josa.of('타워'), '로');
+  });
+
+  test('Exception: 으로', () {
+    final josa = Josa.of('으로/로', withValue: false);
+    // Exception!
+    expect(josa.of('건물'), '로');
   });
 }
